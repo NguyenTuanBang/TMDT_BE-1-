847115a28ae70ee0feec790124185e9f7e34e394
NguyenTuanBang <bang212006@gmail.com>
2025-10-21 21:30:58 +0700

    fix: remove redundant post-save hooks in CartStoreSchema

--- models/CartStoreModel.js
+++ models/CartStoreModel.js
@@ -2,61 +2,96 @@ import mongoose from "mongoose";
 import CartModel from "./CartModel.js";
 import { applyPromotionsToItems } from "../utils/calculateCart.js";
 
-const CartStoreSchema = new mongoose.Schema({
-    cart_id:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Cart"
+const CartStoreSchema = new mongoose.Schema(
+  {
+    cart_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cart",
     },
-    store_id:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Store"
+    store_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Store",
     },
-    promotion:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Promotion"
+    promotion: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Promotion",
     },
-    shippingFee:{
-        type: Number,
-        required: true,
-        default: 0
+    promotionExpiresAt: {
+      type: Date, // lưu thời điểm hết hạn khuyến mãi
+      default: null,
     },
-    subTotal:{
-        type: Number,
-        default: 0
+    shippingFee: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    subTotal: {
+      type: Number,
+      default: 0,
     },
     finalTotal: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     onDeploy: {
-        type: Boolean,
-        default: true
-    }
-},{
-    timestamps: true
-})
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 
-
-CartStoreSchema.pre("save", async function (next) {
-    // Nếu chỉ thay đổi promotion hoặc phí ship, ta trigger lại tính toán
-    if (this.isModified("promotion") || this.isModified("shippingFee")) {
-        await applyPromotionsToItems(this.cart_id, this._id);
-    }
-    next();
+CartStoreSchema.pre("save", function (next) {
+  this._promotionChanged = this.isModified("promotion");
+  this._shippingChanged = this.isModified("shippingFee");
+  this._deployChanged = this.isModified("onDeploy");
+  next();
 });
 
-CartStoreSchema.pre("save", async function (next) {
-    // Nếu chỉ thay đổi trạng thái thì bỏ promotion của store này và nếu toàn bộ store đều bị loại bỏ thì sẽ xoá promotion trong Cart tổng
-    if (this.isModified("onDeploy")) {
-        this.promotion = null
-        const Cart = await CartModel.findById(this.cart_id)
-        const remainingCartStore = await CartStoreModel.find({cart_id: Cart._id})
-        if(!remainingCartStore) Cart.promotion = null
-        await applyPromotionsToItems(this.cart_id);
+CartStoreSchema.post("save", async function (doc) {
+  if (this._promotionChanged) {
+    if (doc.promotion) {
+      doc.promotionExpiresAt = new Date(Date.now() + 5 * 60 * 1000);
+    } else {
+      doc.promotionExpiresAt = null;
     }
-    next();
+    await doc.save(); 
+    await applyPromotionsToItems(doc.cart_id, doc._id);
+  }
 });
 
-const CartStoreModel = mongoose.model("CartStore", CartStoreSchema)
-export default CartStoreModel
+
+CartStoreSchema.post("save", async function (doc) {
+  console.log("CartStore shippingFee modified:", this._shippingChanged);
+  if (this._shippingChanged) {
+    await applyPromotionsToItems(doc.cart_id, doc._id);
+  }
+});
+
+
+CartStoreSchema.post("save", async function (doc) {
+  if (this._deployChanged && doc.onDeploy === false) {
+    doc.promotion = null;
+    await doc.save();
+
+    const Cart = await CartModel.findById(doc.cart_id);
+    const remainingStores = await mongoose.model("CartStore").find({
+      cart_id: Cart._id,
+      onDeploy: true,
+    });
+
+   
+    if (remainingStores.length === 0) {
+      Cart.promotion = null;
+      await Cart.save();
+    }
+
+    await applyPromotionsToItems(doc.cart_id);
+  }
+});
+
+const CartStoreModel = mongoose.model("CartStore", CartStoreSchema);
+export default CartStoreModel;
