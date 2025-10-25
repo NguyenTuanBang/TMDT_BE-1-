9c9fc165c3c5095140b4342291bef0e17cfcd9b6
NguyenTuanBang <bang212006@gmail.com>
2025-10-25 15:48:07 +0700

    feat: integrate SendGrid for email sending functionality

--- utils/mail.js
+++ utils/mail.js
@@ -1,22 +1,69 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: "TMDT <no-reply@TMDT.com>",
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// export default sendEmail;
+// import { Resend } from "resend";
+
+// const sendEmail = async (options) => {
+//   console.log("🚀 ~ file: mail.js:3 ~ Resend:", process.env.RESEND_API_KEY);
+//   const resend = new Resend(process.env.RESEND_API_KEY);
+//   try {
+//     await resend.emails.send({
+//       from: "TMDT <no-reply@tmdt.com>", // hoặc "onboarding@resend.dev" nếu bạn chưa verify domain
+//       to: options.email,
+//       subject: options.subject,
+//       text: options.message,
+//     });
+//   } catch (error) {
+//     console.error("❌ Error sending email:", error);
+//     throw new Error("Failed to send email");
+//   }
+// };
+
+// export default sendEmail;
+
+import sgMail from "@sendgrid/mail";
+
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: "TMDT <no-reply@TMDT.com>",
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
     to: options.email,
+    from: {
+      name: "TMDT",
+      email: process.env.Email, // domain này phải được verify trong SendGrid
+    },
     subject: options.subject,
     text: options.message,
   };
 
-  await transporter.sendMail(mailOptions);
+  try {
+    const response = await sgMail.send(msg);
+    console.log("✅ Email sent:", response[0].statusCode);
+    return response;
+  } catch (error) {
+    console.error("❌ Error sending email:", error.response?.body || error);
+    throw new Error("Failed to send email");
+  }
 };
 
 export default sendEmail;
+
